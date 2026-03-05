f0b8d88171078563f186d1ee8698d967fa1d2cb4
apisak47 <std.66122420222@ubru.ac.th>
2026-03-06 00:38:21 +0700

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    # เชื่อม Relationship ไปยังตารางเงิน
+    transactions = db.relationship('Transaction', backref='owner', lazy=True)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
